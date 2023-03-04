74fc54708eb4f36e6ce5a1b9e190c2b8cd31fd25
burzhun <b.burzhunov@ficto.ru>
2023-03-04 13:21:28 +0300

    add web app

--- index.js
+++ index.js
@@ -1,12 +1,68 @@
-const TelegramApi = require("node-telegram-bot-api");
+const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
+const cors = require("cors");
 
 const token = "6208899312:AAEXw74SiCW0gW-WMtrG7bUZWuscWnF9H5A";
+const webAppUrl = "https://telegram-bot-sklad-front.onrender.com/";
 
-const bot = new TelegramApi(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
+const app = express();
 
-bot.on("message", (msg) => {
-  const text = msg.text;
+app.use(express.json());
+app.use(cors());
+
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "You wrote " + text);
-  console.log(msg);
+  const text = msg.text;
+
+  if (text === "/start") {
+    await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
+      reply_markup: {
+        keyboard: [[{ text: "Заполнить форму", web_app: { url: webAppUrl + "/form" } }]],
+      },
+    });
+
+    await bot.sendMessage(chatId, "Заходи в наш интернет магазин по кнопке ниже", {
+      reply_markup: {
+        inline_keyboard: [[{ text: "Сделать заказ", web_app: { url: webAppUrl } }]],
+      },
+    });
+  }
+
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+      console.log(data);
+      await bot.sendMessage(chatId, "Спасибо за обратную связь!");
+      await bot.sendMessage(chatId, "Ваша страна: " + data?.country);
+      await bot.sendMessage(chatId, "Ваша улица: " + data?.street);
+
+      setTimeout(async () => {
+        await bot.sendMessage(chatId, "Всю информацию вы получите в этом чате");
+      }, 3000);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 });
+
+app.post("/web-data", async (req, res) => {
+  const { queryId, products = [], totalPrice } = req.body;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Успешная покупка",
+      input_message_content: {
+        message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map((item) => item.title).join(", ")}`,
+      },
+    });
+    return res.status(200).json({});
+  } catch (e) {
+    return res.status(500).json({});
+  }
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log("server started on PORT " + PORT));
